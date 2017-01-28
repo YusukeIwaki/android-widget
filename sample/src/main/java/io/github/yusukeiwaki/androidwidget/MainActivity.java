0af9fbd14bd94115e916f04311a236c24036f8d2
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2017-01-29 02:09:04 +0900

    initial commit

---
+++ sample/src/main/java/io/github/yusukeiwaki/androidwidget/MainActivity.java
@@ -0,0 +1,11 @@
+package io.github.yusukeiwaki.androidwidget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
